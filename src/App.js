1647c90942d28b3932e3697465dc5c5010a1c304
lefthomas <lewisfsidw@gmail.com>
2022-11-06 19:47:55 +0000

    corrected row height over extending

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 
 import Main from "./components/Main/Main";
 import SideBar from "./components/SideBar/SideBar";
+import TopNav from "./components/TopNav/TopNav";
 
 function App() {
   return (
     <div className="App">
+      <TopNav />
       <SideBar />
       <Main />
     </div>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,10 @@
 import "./Main.css";
-import TopNav from "../TopNav/TopNav";
 import LocBar from "../LocBar/LocBar";
 import BSQtoLWH from "../NotBooked/NotBooked";
 
 function Main() {
   return (
     <div className="main-div">
-      <TopNav />
       <LocBar />
       <BSQtoLWH />
     </div>
