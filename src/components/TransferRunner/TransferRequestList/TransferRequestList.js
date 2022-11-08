1c8585919cd19fb635763af63aa6f4eda64edc6d
lefthomas <lewisfsidw@gmail.com>
2022-11-08 18:59:43 +0000

    added legends to transfer list border

---
+++ src/components/TransferRunner/TransferRequestList/TransferRequestList.js
@@ -0,0 +1,15 @@
+import "./TransferRequestList.css";
+
+function TransferRequestList() {
+  return (
+    <div>
+      <div class="transfer-request-list-border">
+        <p class="transfer-request-list-department">Legend1</p>
+        <p class="transfer-request-list-toggle">Legend2</p>
+        <p>Form content</p>
+      </div>
+    </div>
+  );
+}
+
+export default TransferRequestList;
