cf8fe95655aebe936504dbc781dd51763d7f172b
lefthomas <lewisfsidw@gmail.com>
2022-11-08 15:09:30 +0000

    added transfer request folder structure

---
+++ src/components/TransferRunner/TransferRunnerBtnBox/TransferRunnerBtnBox.js
@@ -0,0 +1,11 @@
+import "./TransferRunnerBtnBox.css";
+
+function TransferRunnerBtnBox(props) {
+  return (
+    <div class="transfer-runner-btn-container">
+      <button>Print Pick List</button>
+    </div>
+  );
+}
+
+export default TransferRunnerBtnBox;
