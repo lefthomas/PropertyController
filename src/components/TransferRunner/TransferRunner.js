d715c426bb7a15ac817dfb3f284cc383b5252f8f
lefthomas <lewisfsidw@gmail.com>
2022-11-08 15:37:38 +0000

    added transfer btns

--- src/components/TransferRunner/TransferRunner.js
+++ src/components/TransferRunner/TransferRunner.js
@@ -1,5 +1,6 @@
 import "./TransferRunner.css";
 import TransferRunnerInfo from "./TransferRunnerInfo/TransferRunnerInfo";
+import TransferRunnerBtnBox from "./TransferRunnerBtnBox/TransferRunnerBtnBox";
 
 function TransferRunner() {
   return (
@@ -12,7 +13,7 @@ function TransferRunner() {
         additionTime={"4:00pm"}
         additionDate={"27/02/2023"}
       />
-      <div class="transfer-runner-btn-box"></div>
+      <TransferRunnerBtnBox />
       <div class="transfer-runner-request-box"></div>
       <div class="transfer-runner-list">
         <div class="transfer-runner-list-item"></div>
--- src/components/TransferRunner/TransferRunnerBtnBox/TransferRunnerBtnBox.js
+++ src/components/TransferRunner/TransferRunnerBtnBox/TransferRunnerBtnBox.js
@@ -1,9 +1,23 @@
 import "./TransferRunnerBtnBox.css";
 
-function TransferRunnerBtnBox(props) {
+function TransferRunnerBtnBox() {
   return (
     <div class="transfer-runner-btn-container">
-      <button>Print Pick List</button>
+      <button class="transfer-runner-btn transfer-runner-btn-pick">
+        Print Pick List
+      </button>
+      <button class="transfer-runner-btn transfer-runner-btn-email">
+        Email All
+      </button>
+      <button class="transfer-runner-btn transfer-runner-btn-change">
+        Change Transfer Details
+      </button>
+      <button class="transfer-runner-btn transfer-runner-btn-completed">
+        Mark As Completed
+      </button>
+      <button class="transfer-runner-btn transfer-runner-btn-cancel">
+        Cancel Transfer
+      </button>
     </div>
   );
 }
