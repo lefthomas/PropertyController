3db0c59c9cddef06a2e803c8dd6ba1f060939193
lefthomas <lewisfsidw@gmail.com>
2022-11-13 15:05:23 +0000

    added live data ot transfer runner

--- src/components/TransferRunner/TransferRequestListItem/TransferRequestListItem.js
+++ src/components/TransferRunner/TransferRequestListItem/TransferRequestListItem.js
@@ -1,4 +1,17 @@
 import "./TransferRequestListItem.css";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_OBJECT = gql`
+  query GetProperty($amount: Int) {
+    getProperty(amount: $amount) {
+      artist
+      lot
+      objectNumber
+      saleNumber
+      title
+    }
+  }
+`;
 
 function getRandomInt(min, max) {
   min = Math.ceil(2);
@@ -7,20 +20,38 @@ function getRandomInt(min, max) {
 }
 
 function TransferRequestListItem() {
-  return (
-    <div className="transfer-request-list-item-container">
-      <img
-        src={`https://picsum.photos/id/${getRandomInt()}/100`}
-        alt=""
-        className="transfer-request-list-item-img"
-      />
+  const { loading, error, data } = useQuery(GET_OBJECT, {
+    variables: { amount: 3 },
+  });
+  if (loading) return <p>Loading...</p>;
 
-      <p className="transfer-request-list-item-">Object Information</p>
-      <p className="transfer-request-list-item-">Artist Title</p>
-      <p className="transfer-request-list-item-">Size</p>
-      <p className="transfer-request-list-item-">Location</p>
-      <p className="transfer-request-list-item-">Requested by</p>
-    </div>
+  console.log(data);
+  if (error) return <p>Error</p>;
+
+  return data.getProperty.map(
+    ({ artist, lot, objectNumber, saleNumber, title }) => (
+      <div className="transfer-request-list-item-container">
+        <img
+          src={`https://picsum.photos/id/${getRandomInt()}/100`}
+          alt=""
+          className="transfer-request-list-item-img"
+        />
+        <p className="transfer-request-list-item-">
+          Sale: {saleNumber} <br />
+          Lot: {lot} <br />
+          Object No: {objectNumber}
+        </p>
+        <p className="transfer-request-list-item-">
+          {artist} <br />
+          {title}
+        </p>
+        <p className="transfer-request-list-item-">Size</p>
+        <p className="transfer-request-list-item-">Location</p>
+        <p className="transfer-request-list-item-">
+          Requested by <br /> Lewis Thomas
+        </p>
+      </div>
+    )
   );
 }
 
