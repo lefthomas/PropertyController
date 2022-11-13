fd73b8c4c45b17e5098d4bb55c38be8612dea8db
lefthomas <lewisfsidw@gmail.com>
2022-11-13 15:51:37 +0000

    added transfer portal

--- src/components/TransferRunner/TransferRunnerRequestBox/TransferRunnerRequestBox.js
+++ src/components/TransferRunner/TransferRunnerRequestBox/TransferRunnerRequestBox.js
@@ -4,14 +4,27 @@ import React, { useState } from "react";
 
 function TransferRunnerRequestBox() {
   const [isOpen, setIsOpen] = useState(false);
-  const handleClose = () => setIsOpen(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+    // Disables Background Scrolling whilst the SideDrawer/Modal is open
+    if (typeof window != "undefined" && window.document) {
+      document.body.style.overflow = "hidden";
+    }
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+    // Unsets Background Scrolling to use when SideDrawer/Modal is closed
+    document.body.style.overflow = "unset";
+  };
+
   return (
     <div className="transfer-runner-request-container">
       <button className="transfer-runner-request-btn transfer-runner-request-btn-select">
         Select
       </button>
       <button
-        onClick={() => setIsOpen(true)}
+        onClick={handleOpen}
         className="transfer-runner-request-btn transfer-runner-request-btn-request"
       >
         Request Work
--- src/modals/TransferModal/TransferModal.js
+++ src/modals/TransferModal/TransferModal.js
@@ -1,36 +1,39 @@
 import "./TransferModal.css";
-import React, { useState } from "react";
+import ReactDom from "react-dom";
 
 function TransferModal(props) {
   if (!props.open) return null;
-  return (
-    <form className="transfer-request-list-border">
-      <label>
-        Shipper:
-        <input name="transfer-shipper" />
-      </label>
-      <label>
-        Coordinator:
-        <input name="transfer-coordinator" />
-      </label>
-      <label>
-        Collection Date:
-        <input type="date" name="collection-date" />
-      </label>
-      <label>
-        Collection Time:
-        <input type="time" name="collection-time" />
-      </label>
-      <label>
-        Delivery Date:
-        <input type="date" name="delivery-date" />
-      </label>
-      <label>
-        Delivery Time:
-        <input type="time" name="delivery-time" />
-      </label>
-      <button onClick={() => props.close()}>Close</button>
-    </form>
+  return ReactDom.createPortal(
+    <div className="transfer-request-list-overlay">
+      <form className="transfer-request-list-form">
+        <label>
+          Shipper:
+          <input name="transfer-shipper" />
+        </label>
+        <label>
+          Coordinator:
+          <input name="transfer-coordinator" />
+        </label>
+        <label>
+          deadline for additions date:
+          <input type="date" name="additions-date" />
+        </label>
+        <label>
+          Deadline for additions time:
+          <input type="time" name="additions-time" />
+        </label>
+        <label>
+          Delivery Date:
+          <input type="date" name="delivery-date" />
+        </label>
+        <label>
+          Delivery Time:
+          <input type="time" name="delivery-time" />
+        </label>
+        <button onClick={() => props.close()}>Close</button>
+      </form>
+    </div>,
+    document.getElementById("portal")
   );
 }
 
