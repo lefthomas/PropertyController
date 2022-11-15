530ab07b6890153a9fd42fcbae2c8bd3d0289bbe
lefthomas <lewisfsidw@gmail.com>
2022-11-15 15:09:20 +0000

    moved transfer modal to notbooked button

--- src/components/NotBooked/NotBooked.js
+++ src/components/NotBooked/NotBooked.js
@@ -1,10 +1,28 @@
 import "./NotBooked.css";
+import React, { useState } from "react";
+import TransferModal from "../../modals/TransferModal/TransferModal";
 
 function NotBooked() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+    // Disables Background Scrolling whilst the SideDrawer/Modal is open
+    if (typeof window != "undefined" && window.document) {
+      document.body.style.overflow = "hidden";
+    }
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+    // Unsets Background Scrolling to use when SideDrawer/Modal is closed
+    document.body.style.overflow = "unset";
+  };
   return (
     <div className="not-booked-container">
-      <p className="not-booked-info">Nothing Scheduled</p>
-      <button className="not-booked-btn">Schedule New Transfer</button>
+      <button className="not-booked-btn" onClick={handleOpen}>
+        Schedule New Transfer
+      </button>
+      <TransferModal open={isOpen} close={handleClose} />
     </div>
   );
 }
--- src/components/TransferRunner/TransferRunnerRequestBox/TransferRunnerRequestBox.js
+++ src/components/TransferRunner/TransferRunnerRequestBox/TransferRunnerRequestBox.js
@@ -1,38 +1,17 @@
-import TransferModal from "../../../modals/TransferModal/TransferModal";
 import "./TransferRunnerRequestBox.css";
-import React, { useState } from "react";
 
 function TransferRunnerRequestBox() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOpen = () => {
-    setIsOpen(true);
-    // Disables Background Scrolling whilst the SideDrawer/Modal is open
-    if (typeof window != "undefined" && window.document) {
-      document.body.style.overflow = "hidden";
-    }
-  };
-  const handleClose = () => {
-    setIsOpen(false);
-    // Unsets Background Scrolling to use when SideDrawer/Modal is closed
-    document.body.style.overflow = "unset";
-  };
-
   return (
     <div className="transfer-runner-request-container">
       <button className="transfer-runner-request-btn transfer-runner-request-btn-select">
         Select
       </button>
-      <button
-        onClick={handleOpen}
-        className="transfer-runner-request-btn transfer-runner-request-btn-request"
-      >
+      <button className="transfer-runner-request-btn transfer-runner-request-btn-request">
         Request Work
       </button>
       <button className="transfer-runner-request-btn transfer-runner-request-btn-toggle">
         Expand / Collapse All
       </button>
-      <TransferModal open={isOpen} close={handleClose} />
     </div>
   );
 }
