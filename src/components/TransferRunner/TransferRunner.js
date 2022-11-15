04460d06829baaabd374837fb1a34459709f4d07
lefthomas <lewisfsidw@gmail.com>
2022-11-15 17:35:39 +0000

    made TransferRunner dynamic

--- src/components/TransferRunner/TransferRunner.js
+++ src/components/TransferRunner/TransferRunner.js
@@ -3,6 +3,18 @@ import TransferRunnerInfo from "./TransferRunnerInfo/TransferRunnerInfo";
 import TransferRunnerBtnBox from "./TransferRunnerBtnBox/TransferRunnerBtnBox";
 import TransferRunnerRequestBox from "./TransferRunnerRequestBox/TransferRunnerRequestBox";
 import TransferRequestList from "./TransferRequestList/TransferRequestList";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_TRANSFERS = gql`
+  query GetTransfers {
+    getTransfers {
+      shipper
+      coordinator
+      additionsDate
+      departureDate
+    }
+  }
+`;
 
 function TransferRunner() {
   // TODO add in handling for nothing booked
@@ -10,20 +22,26 @@ function TransferRunner() {
   // if (!transferID && transfer.complete = false) return(
   //   <p className="not-booked-info">Nothing Scheduled</p>
   // );
-  return (
-    <div className="transfer-runner-container">
-      <TransferRunnerInfo
-        coordinator={"Lewis Thomas"}
-        shipper={"Met"}
-        travelTime={"9:00am"}
-        travelDate={"28/02/2023"}
-        additionTime={"4:00pm"}
-        additionDate={"27/02/2023"}
-      />
-      <TransferRunnerBtnBox />
-      <TransferRunnerRequestBox />
-      <TransferRequestList />
-    </div>
+  const { loading, error, data } = useQuery(GET_TRANSFERS);
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error</p>;
+  return data.getTransfers.map(
+    ({ shipper, coordinator, additionsDate, departureDate }) => (
+      <div className="transfer-runner-container">
+        <TransferRunnerInfo
+          coordinator={coordinator}
+          shipper={shipper}
+          travelTime={additionsDate}
+          travelDate={departureDate}
+          additionTime={additionsDate}
+          additionDate={departureDate}
+        />
+        <TransferRunnerBtnBox />
+        <TransferRunnerRequestBox />
+        <TransferRequestList />
+      </div>
+    )
   );
 }
 
