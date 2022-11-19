0c10a37e7c5c4a11165ae3431f39c3b2ec20d41d
lefthomas <lewisfsidw@gmail.com>
2022-11-19 13:25:19 +0000

    added colon

--- src/components/TransferRunner/TransferRunnerInfo/TransferRunnerInfo.js
+++ src/components/TransferRunner/TransferRunnerInfo/TransferRunnerInfo.js
@@ -9,7 +9,7 @@ function TransferRunnerInfo(props) {
   return (
     <div className="transfer-runner-info">
       <h1>{props.departureDate}</h1>
-      <p>Coordinator {props.coordinator}</p>
+      <p>Coordinator: {props.coordinator}</p>
       <p>Transfer Booked via {props.shipper}</p>
       <p>
         Departing at {departureTime} on {departureDate}
