d10f89ca2d0482e49c965d67c2df262ba2b59987
lefthomas <lewisfsidw@gmail.com>
2022-11-21 20:32:16 +0000

    framework for property request modal

--- src/components/TransferRunner/TransferRunnerRequestBox/TransferRunnerRequestBox.js
+++ src/components/TransferRunner/TransferRunnerRequestBox/TransferRunnerRequestBox.js
@@ -1,14 +1,35 @@
+import AddPropertyModal from "../../../modals/AddPropertyModal/AddPropertyModal";
+import React, { useState } from "react";
 import "./TransferRunnerRequestBox.css";
 
 function TransferRunnerRequestBox() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+    // Disables Background Scrolling whilst the SideDrawer/Modal is open
+    if (typeof window != "undefined" && window.document) {
+      document.body.style.overflow = "hidden";
+    }
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+    // Unsets Background Scrolling to use when SideDrawer/Modal is closed
+    document.body.style.overflow = "unset";
+  };
+
   return (
     <div className="transfer-runner-request-container">
       <button className="transfer-runner-request-btn transfer-runner-request-btn-select">
         Select
       </button>
-      <button className="transfer-runner-request-btn transfer-runner-request-btn-request">
+      <button
+        className="transfer-runner-request-btn transfer-runner-request-btn-request"
+        onClick={handleOpen}
+      >
         Request Work
       </button>
+      <AddPropertyModal open={isOpen} close={handleClose} />
       <button className="transfer-runner-request-btn transfer-runner-request-btn-toggle">
         Expand / Collapse All
       </button>
---
+++ src/modals/AddPropertyModal/AddPropertyModal.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./AddPropertyModal.css";
+import ReactDom from "react-dom";
+
+function AddPropertyModal(props) {
+  if (!props.open) return null;
+  return ReactDom.createPortal(
+    // Form tag was causing an inconsistent focus error in Chrome that was causing submit to fail randomly so changed to div
+
+    <div className="transfer-modal-overlay">
+      <div className="transfer-modal-container">
+        <h2 className="transfer-modal-header">Request Property for Transfer</h2>
+
+        <div className="add-property-modal-select-container">
+          <button>Search Inventory</button>
+          <button>Not on Inventory</button>
+          <div className="add-property-modal-on-inventory-input-view">
+            <label for="add-property-modal-search-by">Search by:</label>
+            <select
+              name="add-property-modal-search-by"
+              id="add-property-modal-search-by"
+            >
+              <option value="object-number">Object Number</option>
+
+              <option value="sale-code">Sale Code</option>
+            </select>
+          </div>
+
+          <div className="add-property-modal-not-on-inventory-input-view"></div>
+        </div>
+
+        <div className="add-property-modal-staged-property-view"></div>
+
+        <button
+          type="submit"
+          className="transfer-modal-btn transfer-modal-btn-confirm"
+        >
+          Confirm
+        </button>
+        <button
+          type="button"
+          className="transfer-modal-btn transfer-modal-btn-cancel"
+          onClick={() => props.close()}
+        >
+          Cancel
+        </button>
+      </div>
+    </div>,
+    document.getElementById("portal")
+  );
+}
+
+export default AddPropertyModal;
