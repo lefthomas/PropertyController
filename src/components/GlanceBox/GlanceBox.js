15afe686e683454a33be4e0a58f51cad38e0814c
lefthomas <lewisfsidw@gmail.com>
2022-11-22 17:29:58 +0000

    made glancebox dynamic

--- src/components/GlanceBox/GlanceBox.js
+++ src/components/GlanceBox/GlanceBox.js
@@ -1,12 +1,25 @@
 import "./GlanceBox.css";
 
 function GlanceBox(props) {
-  return (
-    <div className="glance-box">
-      <p className="glance-box-direction">Next truck {props.direction}</p>
-      <p className="glance-box-not-booked">No Transfer currently scheduled</p>
-      <button className="glance-box-btn">Book Transfer</button>
-    </div>
+  if (props.transferInfo.length === 0)
+    return (
+      <div className="glance-box">
+        <p className="glance-box-direction">Next truck {props.direction}</p>
+        <p className="glance-box-not-booked">No Transfer currently scheduled</p>
+        <button className="glance-box-btn">Book Transfer</button>
+      </div>
+    );
+  return props.transferInfo.map(
+    ({ shipper, additionsDate, coordinator, _id }) => (
+      <div className="glance-box" key={_id}>
+        <p className="glance-box-direction">Next truck {props.direction}</p>
+        <p className="glance-box-text">
+          {shipper} on {additionsDate}
+        </p>
+        <p className="glance-box-text">Coordinator: {coordinator}</p>
+        <button className="glance-box-btn">Request Property</button>
+      </div>
+    )
   );
 }
 
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,12 +1,37 @@
 import "./SideBar.css";
 import GlanceBox from "../GlanceBox/GlanceBox";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_GLANCE_BOX = gql`
+  query GetGlanceBox($LWH: String, $BSQ: String) {
+    LWH: getGlanceBox(originLocation: $LWH) {
+      additionsDate
+      coordinator
+      shipper
+      _id
+    }
+    BSQ: getGlanceBox(originLocation: $BSQ) {
+      additionsDate
+      coordinator
+      shipper
+      _id
+    }
+  }
+`;
 
 function SideBar() {
+  const { loading, error, data } = useQuery(GET_GLANCE_BOX, {
+    variables: { LWH: "LWH", BSQ: "BSQ" },
+  });
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error</p>;
+
   return (
     <div className="side-bar">
-      <p className="side-bar-title">At a Glance</p>
-      <GlanceBox direction={"BSQ to LWH"} />
-      <GlanceBox direction={"LWH to BSQ"} />
+      <GlanceBox direction={"BSQ to LWH"} transferInfo={data.BSQ} />
+      <GlanceBox direction={"LWH to BSQ"} transferInfo={data.LWH} />
     </div>
   );
 }
