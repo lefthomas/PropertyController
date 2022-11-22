6154caeee64bd843a86aa80192051609b77b8d26
lefthomas <lewisfsidw@gmail.com>
2022-11-22 17:30:13 +0000

    added check for null

--- src/components/TransferRunner/TransferRunner.js
+++ src/components/TransferRunner/TransferRunner.js
@@ -21,27 +21,16 @@ const GET_TRANSFERS = gql`
 function TransferRunner() {
   const { loading, error, data } = useQuery(GET_TRANSFERS);
 
-  // function transferCompleteBooleanCheck() {
-  //   data.getTransfers.filter((transfer) => {
-  //     const complete = transfer.complete;
-  //     return complete;
-  //   });
-  // }
-
   if (loading) return <p>Loading...</p>;
 
   if (error) return <p>Error</p>;
 
-  // if (
-  //   data.getTransfers === undefined ||
-  //   transferCompleteBooleanCheck() === true
-  // )
-  // return;
-  // <p className="not-booked-info">Nothing Scheduled</p>;
+  if (data.getTransfers.length === 0)
+    return <p className="not-booked-info">Nothing Scheduled</p>;
 
   return data.getTransfers.map(
     ({ shipper, coordinator, additionsDate, departureDate, _id }) => (
-      <div className="transfer-runner-container">
+      <div className="transfer-runner-container" key={_id}>
         <TransferRunnerInfo
           coordinator={coordinator}
           shipper={shipper}
