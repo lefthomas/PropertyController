cc347afa9e503f37d36f062ce691579b149426b7
lefthomas <lewisfsidw@gmail.com>
2023-01-10 17:59:25 +0000

    push for rebuild

--- src/modals/TransferModal/TransferModal.js
+++ src/modals/TransferModal/TransferModal.js
@@ -88,7 +88,7 @@ function TransferModal(props) {
               className="transfer-modal-label"
               htmlFor="transfer-modal-coordinator"
             >
-              Coordinator
+              Coordinators
             </p>
             <input
               name="transfer-modal-coordinator"
