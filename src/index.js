2f6ae8062b2f8ac25648496872e3ba7f080dda27
lefthomas <lewisfsidw@gmail.com>
2023-03-09 22:36:20 +0000

    change db link

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/authContext";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5001/",
+  uri: "https://property-controller-db.herokuapp.com/",
 });
 
 const authLink = setContext((_, { headers }) => {
