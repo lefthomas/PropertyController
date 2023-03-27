a77712a8250f4de6a55cf911e8ae780ed61a1476
lefthomas <lewisfsidw@gmail.com>
2023-03-27 17:10:10 +0100

    add glacebox property modal

--- src/components/GlanceBox/GlanceBox.js
+++ src/components/GlanceBox/GlanceBox.js
@@ -1,7 +1,24 @@
 import "./GlanceBox.css";
 import BookTransfer from "../BookTransfer/BookTransfer";
+import AddPropertyModal from "../../modals/AddPropertyModal/AddPropertyModal";
+import { useState } from "react";
 
 function GlanceBox({ direction, transferInfo, originLoc }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+    // Disables Background Scrolling whilst the SideDrawer/Modal is open
+    if (typeof window != "undefined" && window.document) {
+      document.body.style.overflow = "hidden";
+    }
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+    // Unsets Background Scrolling to use when SideDrawer/Modal is closed
+    document.body.style.overflow = "unset";
+  };
+
   if (transferInfo.length === 0)
     return (
       <div className="glance-box">
@@ -17,7 +34,10 @@ function GlanceBox({ direction, transferInfo, originLoc }) {
         {shipper} on {additionsDate}
       </p>
       <p className="glance-box-text">Coordinator: {coordinator}</p>
-      <button className="glance-box-btn">Request Property</button>
+      <button className="glance-box-btn" onClick={handleOpen}>
+        Request Work
+      </button>
+      <AddPropertyModal ID={_id} open={isOpen} close={handleClose} />
     </div>
   ));
 }
